26ea90415ae517af61d98b501ccc5ca1ab099c1d
Paul Ponmattam <paul.g.ponmattam@gmail.com>
2025-10-19 00:34:44 -0400

    clean up test

--- test/test_value.py
+++ test/test_value.py
@@ -1,4 +1,4 @@
-from src.libvalue import Value, Neuron, Layer, MLP
+from src.libvalue import Value
 
 def test_basic():
     x = Value(2)
@@ -9,9 +9,6 @@ def test_basic():
     print(f"{x.describe()}")
     print(f"{y.describe()}")
 
-    n = Neuron(input_size=10)
-    l = Layer(input_size=10, output_size=5)
-    mlp = MLP(input_size=1, output_sizes=[1])
 
 if __name__ == "__main__":
     test_basic()
